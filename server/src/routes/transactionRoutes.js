86cd5317d3322d1b0926870363bda89324fb1883
Alex Shatokhin <shatokhin.04@inbox.ru>
2025-01-09 10:41:26 +0200

    add updateTransaction

--- server/src/routes/transactionRoutes.js
+++ server/src/routes/transactionRoutes.js
@@ -4,7 +4,7 @@ const transactionService = require("../services/transactionService");
 
 routes.post("/transactions", authMiddleware, transactionService.postTransaction);
 routes.get("/transactions", authMiddleware, transactionService.getTransactions)
-routes.put("/transactions/:transactionID", authMiddleware, (req, res) => {res.status(200).json({message: "successfull"})});
+routes.put("/transactions/:transactionID", authMiddleware, transactionService.updateTransaction);
 routes.delete("/transactions/:transactionID", authMiddleware, transactionService.deleteTransaction);
 
 module.exports = routes;
