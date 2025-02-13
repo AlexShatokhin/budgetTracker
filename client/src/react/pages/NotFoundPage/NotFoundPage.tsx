95a2f3e825b1cd0be93c8a6005c7cd94d6d57d0b
Alex Shatokhin <shatokhin.04@inbox.ru>
2025-02-13 11:22:53 +0200

    design 404 page

--- client/src/react/pages/NotFoundPage/NotFoundPage.tsx
+++ client/src/react/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,20 @@
+import NotFoundImage from "../../../assets/404.png";
+import { FaChevronLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-
+import "./not_found.scss"
 const NotFoundPage = () => {
     return (
         <div className="error">
-            <h1>404</h1>
-            <h2>Page not found</h2>
+            <img src={NotFoundImage} alt="404 Not Found" className="error__image" />
+            <div className="error__text">
+                <h1>Oops!</h1>
+                <h2>We couldn't find the page <br /> you were looking for</h2>
+                <Link to="/home">
+                    <FaChevronLeft className="error__icon" />
+                    Go home
+                </Link>
+            </div>
         </div>
     )
 }
