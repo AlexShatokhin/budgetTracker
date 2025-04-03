31bde9e10f85cccda6d46a0a481ce6e1045a4c8b
Alex Shatokhin <shatokhin.04@inbox.ru>
2025-04-03 10:31:41 +0300

    update report page wrapper title

--- client/src/react/pages/ReportPage/ReportPage.tsx
+++ client/src/react/pages/ReportPage/ReportPage.tsx
@@ -45,7 +45,7 @@ const ReportPage = () => {
                 </div>
 
                 <div className="reports-content">
-                    <Wrapper title="Categoeies" width="calc(100% - 90px)" height="550px">
+                    <Wrapper title="Categories transactions" titleInfo="Click on the chart item to find out the transactions for that category " width="calc(100% - 90px)" height="550px">
                         <CategoriesTransaction type={selectedAmountType} timeFormat={selectedOption!.value || "month"}/>
                     </Wrapper>
                 </div>
