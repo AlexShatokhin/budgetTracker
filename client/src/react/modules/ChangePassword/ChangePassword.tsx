f3365e3b83f1bb9445cca3253b5dd43fb99f7ffc
Alex Shatokhin <shatokhin.04@inbox.ru>
2025-05-16 09:04:59 +0300

    fully add changing password functional

--- client/src/react/modules/ChangePassword/ChangePassword.tsx
+++ client/src/react/modules/ChangePassword/ChangePassword.tsx
@@ -9,6 +9,7 @@ const ChangePassword = () => {
     const [newPassword, setNewPassword] = useState<string>("");
     const [repeatPassword, setRepeatPassword] = useState<string>("");
     const [errorMessage, setErrorMessage] = useState<string>("");
+    const [isChangingSuccess, setIsChangingSuccess] = useState<boolean>(false);
     const [data] = useChangePasswordMutation();
 
     const handleSubmit = () => {
@@ -22,12 +23,13 @@ const ChangePassword = () => {
         }
 
         data({oldPassword, newPassword}).unwrap()
-            .then(e => console.log(e))
-            .catch((e: {status: number, data: {message: string}}) => {console.log(e); setErrorMessage(e.data.message)});
+            .then(e => {setIsChangingSuccess(true); setOldPassword(""); setNewPassword(""); setRepeatPassword(""); setErrorMessage(e.message)})
+            .catch((e: {status: number, data: {message: string}}) => {setIsChangingSuccess(false); setErrorMessage(e.data.message)});
     }
 
     const handleInputChange = (value: string, callback: (value: string) => void) => {
         setErrorMessage("");
+        setIsChangingSuccess(false);
         callback(value);
     }
 
@@ -60,7 +62,7 @@ const ChangePassword = () => {
             <Button
                 onClick={handleSubmit}
                 title="Change password"/>
-                <div className="password-error">
+                <div className={"password-message " + (isChangingSuccess ? "success-message" : "error-message")}>
                     {errorMessage}
                 </div>
         </>
--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -63,12 +63,12 @@ class authService {
         if(oldPassword === newPassword) return res.status(400).json({message: 'New password cannot be same as old password'});
         if(newPassword.length < 6) return res.status(400).json({message: 'New password must be at least 6 characters long'});
 
-        const hashOldPassword = await bcrypt.hash(oldPassword, 10);
         const user = await this.prisma.users.findUnique({
             where: {id: req.userID}
         })
         if(!user) return res.status(400).json({message: 'User not found'});
-        if(user.password !== hashOldPassword) return res.status(400).json({message: 'Invalid credentials'});
+        const isPasswordValid = await bcrypt.compare(oldPassword, user.password);
+        if(!isPasswordValid) return res.status(400).json({message: 'Invalid credentials'});
 
         const hashNewPassword = await bcrypt.hash(newPassword, 10);
         await this.prisma.users.update({
